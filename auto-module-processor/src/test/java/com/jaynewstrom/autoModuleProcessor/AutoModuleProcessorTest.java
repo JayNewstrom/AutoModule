e99ff43205892602133de09d359c81a3e2df5c26
Jay Newstrom <jaynewstrom@gmail.com>
2016-03-30 12:25:48 -0500

    Make generated code prettier This makes it so an import is generated for the `addsTo`, if needed.

--- auto-module-processor/src/test/java/com/jaynewstrom/autoModuleProcessor/AutoModuleProcessorTest.java
+++ auto-module-processor/src/test/java/com/jaynewstrom/autoModuleProcessor/AutoModuleProcessorTest.java
@@ -1,6 +1,5 @@
 package com.jaynewstrom.autoModuleProcessor;
 
-import com.google.common.base.Joiner;
 import com.google.testing.compile.JavaFileObjects;
 
 import org.junit.Test;
@@ -9,72 +8,109 @@ import javax.tools.JavaFileObject;
 
 import static com.google.common.truth.Truth.assertAbout;
 import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
+import static com.google.testing.compile.JavaSourcesSubjectFactory.javaSources;
+import static java.util.Arrays.asList;
 
 public final class AutoModuleProcessorTest {
 
     @Test public void testProcessorWithoutAddsTo() {
-        JavaFileObject sampleActivity = JavaFileObjects.forSourceString("com.example.MainActivity",
-                Joiner.on('\n').join(
-                        "package com.example;",
-                        "import com.jaynewstrom.autoModule.AutoModule;",
-                        "@AutoModule",
-                        "public final class MainActivity {",
-                        "}"
-                )
+        JavaFileObject sampleActivity = JavaFileObjects.forSourceLines("com.example.MainActivity",
+                "package com.example;",
+                "import com.jaynewstrom.autoModule.AutoModule;",
+                "@AutoModule",
+                "public final class MainActivity {",
+                "}"
         );
 
-        JavaFileObject expectedSource = JavaFileObjects.forSourceString("MainActivityModule",
-                Joiner.on('\n').join(
-                        "package com.example;",
-                        "",
-                        "import dagger.Module;",
-                        "",
-                        "@Module(",
-                        "    injects = MainActivity.class,",
-                        "    addsTo = java.lang.Void.class",
-                        ")",
-                        "final class MainActivityModule {",
-                        "}"));
+        JavaFileObject expectedSource = JavaFileObjects.forSourceLines("MainActivityModule",
+                "package com.example;",
+                "",
+                "import dagger.Module;",
+                "import java.lang.Void;",
+                "",
+                "@Module(",
+                "    injects = MainActivity.class,",
+                "    addsTo = Void.class",
+                ")",
+                "final class MainActivityModule {",
+                "}"
+        );
 
         assertAbout(javaSource())
-                 .that(sampleActivity)
-                 .processedWith(new AutoModuleProcessor())
-                 .compilesWithoutError()
-                 .and()
-                 .generatesSources(expectedSource);
+                .that(sampleActivity)
+                .processedWith(new AutoModuleProcessor())
+                .compilesWithoutError()
+                .and()
+                .generatesSources(expectedSource);
     }
 
     @Test public void testProcessorWithAddsTo() {
-        JavaFileObject sampleActivity = JavaFileObjects.forSourceString("com.example.MainActivity",
-                Joiner.on('\n').join(
-                        "package com.example;",
-                        "import com.jaynewstrom.autoModule.AutoModule;",
-                        "import com.example.MainActivity.BaseModule;",
-                        "@AutoModule(addsTo = BaseModule.class)",
-                        "public final class MainActivity {",
-                        "static final class BaseModule {}",
-                        "}"
-                )
+        JavaFileObject sampleActivity = JavaFileObjects.forSourceLines("com.example.MainActivity",
+                "package com.example;",
+                "import com.jaynewstrom.autoModule.AutoModule;",
+                "import com.example.MainActivity.BaseModule;",
+                "@AutoModule(addsTo = BaseModule.class)",
+                "public final class MainActivity {",
+                "static final class BaseModule {}",
+                "}"
         );
 
-        JavaFileObject expectedSource = JavaFileObjects.forSourceString("MainActivityModule",
-                Joiner.on('\n').join(
-                        "package com.example;",
-                        "",
-                        "import dagger.Module;",
-                        "",
-                        "@Module(",
-                        "    injects = MainActivity.class,",
-                        "    addsTo = com.example.MainActivity.BaseModule.class",
-                        ")",
-                        "final class MainActivityModule {",
-                        "}"));
+        JavaFileObject expectedSource = JavaFileObjects.forSourceLines("MainActivityModule",
+                "package com.example;",
+                "",
+                "import dagger.Module;",
+                "",
+                "@Module(",
+                "    injects = MainActivity.class,",
+                "    addsTo = MainActivity.BaseModule.class",
+                ")",
+                "final class MainActivityModule {",
+                "}"
+        );
 
         assertAbout(javaSource())
-                 .that(sampleActivity)
-                 .processedWith(new AutoModuleProcessor())
-                 .compilesWithoutError()
-                 .and()
-                 .generatesSources(expectedSource);
+                .that(sampleActivity)
+                .processedWith(new AutoModuleProcessor())
+                .compilesWithoutError()
+                .and()
+                .generatesSources(expectedSource);
+    }
+
+    @Test public void testProcessorWithAddsToModuleInAnotherPackage() {
+        JavaFileObject sampleActivity = JavaFileObjects.forSourceLines("com.example.MainActivity",
+                "package com.example;",
+                "import com.jaynewstrom.autoModule.AutoModule;",
+                "import com.example.extra.BaseModule;",
+                "@AutoModule(addsTo = BaseModule.class)",
+                "public final class MainActivity {",
+                "}"
+        );
+
+        JavaFileObject baseModule = JavaFileObjects.forSourceLines("com.example.extra.BaseModule",
+                "package com.example.extra;",
+                "public final class BaseModule {",
+                "}"
+        );
+
+        JavaFileObject expectedSource = JavaFileObjects.forSourceLines("MainActivityModule",
+                "package com.example;",
+                "",
+                "import com.example.extra.BaseModule;",
+                "import dagger.Module;",
+                "",
+                "@Module(",
+                "    injects = MainActivity.class,",
+                "    addsTo = BaseModule.class",
+                ")",
+                "final class MainActivityModule {",
+                "}"
+        );
+
+        assertAbout(javaSources())
+                .that(asList(baseModule, sampleActivity))
+                .processedWith(new AutoModuleProcessor())
+                .compilesWithoutError()
+                .and()
+                .generatesSources(expectedSource);
     }
 }
